5c0a8a6a99bd14e5bcec12c8c5ec7fc084de16be
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-06-18 02:51:01 -0600

    figured out how to convert image into an array of RGBA vals
    wow wowo wowowo

---
+++ imageArray.js
@@ -0,0 +1,14 @@
+var getPixels = require("get-pixels")
+ 
+getPixels("logo.png", function(err, pixels) {
+  if(err) {
+    console.log("Bad image path")
+    return
+  }
+  let testArr =[];
+  for(let i = 0; i<pixels.data.length; i++){
+    testArr.push(pixels.data[i]+" "+pixels.data[i+1]+" "+pixels.data[i+2]+" "+pixels.data[i+3]);
+    i+=4;
+  }
+  console.log("got pixels", testArr)
+})
