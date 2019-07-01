827d52390d68af4db09054827e9e45ec6b7eec0b
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-07-01 03:52:10 -0600

    Image Hashing---some progress
    Figured out how to hash the images in base 10 properly...need to make the Jimp.read async and seperate functions. having trouble sorting the array of objects and then logging the sorted array.
    -MSL

--- snowFinder.js
+++ snowFinder.js
@@ -6,36 +6,54 @@ function getName(num){
     let parsedNarr = JSON.parse(nameArr);
     return parsedNarr[num];
   }
-var brightestArr = [];
-var hasBlue = false;
-Jimp.read("./aspen/2019+06Jun+10+18+0+0Sen[TR].png", function (err, image) {
-    for(let i = 0; i<image.bitmap.height; i++){
-        for(let j =0;j<image.bitmap.width; j++){
-        let combColor = Jimp.intToRGBA(image.getPixelColor(j, i))['r'] + Jimp.intToRGBA(image.getPixelColor(j, i))['g']+Jimp.intToRGBA(image.getPixelColor(j, i))['b'];
-            brightestArr.push({totalRGB:combColor,x:j,y:i});
-            image.setPixelColor(0, j, i); // sets the colour of that pixel
+async function blueCheck(){
+    var brightestArr = [];
+    var hasBlue = false;
+    await Jimp.read("./aspen/2019+06Jun+10+18+0+0Sen[TR].png", function (err, image) {
+        for(let i = 0; i<image.bitmap.height; i++){
+            for(let j =0;j<image.bitmap.width; j++){
+            let combColor = Jimp.intToRGBA(image.getPixelColor(j, i))['r'] + Jimp.intToRGBA(image.getPixelColor(j, i))['g']+Jimp.intToRGBA(image.getPixelColor(j, i))['b'];
+                brightestArr.push({totalRGB:combColor,x:j,y:i});
+                image.setPixelColor(0, j, i); // sets the colour of that pixel
 
-       if(Jimp.intToRGBA(image.getPixelColor(j, i))['r']<=156&&Jimp.intToRGBA(image.getPixelColor(j, i))['r']>=127&&Jimp.intToRGBA(image.getPixelColor(j, i))['g']>=115&&Jimp.intToRGBA(image.getPixelColor(j, i))['g']<=155&&Jimp.intToRGBA(image.getPixelColor(j, i))['b']>=126&&Jimp.intToRGBA(image.getPixelColor(j, i))['b']<=162){
-           hasBlue = true;
-           
+        if(Jimp.intToRGBA(image.getPixelColor(j, i))['r']<=156&&Jimp.intToRGBA(image.getPixelColor(j, i))['r']>=127&&Jimp.intToRGBA(image.getPixelColor(j, i))['g']>=115&&Jimp.intToRGBA(image.getPixelColor(j, i))['g']<=155&&Jimp.intToRGBA(image.getPixelColor(j, i))['b']>=126&&Jimp.intToRGBA(image.getPixelColor(j, i))['b']<=162){
+            hasBlue = true;
+            
 
-       }
-       }
-    }
-    brightestArr.sort(function(b,a){return a.totalRGB - b.totalRGB});
-    console.log(hasBlue);
-});
-let nameArrL = fs.readFileSync('./aspen/nameArray.json');
-
-//for(let i =0; i<nameArrL.length; i++){
-    Jimp.read("./aspen/"+getName(0)+".png", function (err, image){
-        console.log(image);
-        hashArr.push({
-            hash: image.hash(10),
-            name: getName(0)
-
-        });
+        }
+        }
+        }
+        brightestArr.sort(function(b,a){return a.totalRGB - b.totalRGB});
+        console.log(hasBlue);
     });
-//}
-hashArr.sort(function(b,a){return a.hash - b.hash});
-console.log(hashArr);
+
+}
+
+async function hashAnalysis(){
+    let nameArrL = fs.readFileSync('./aspen/nameArray.json');
+    let parsedNarrL = JSON.parse(nameArrL);
+
+    console.log(parsedNarrL.length);
+        for(let i = 0; i<parsedNarrL.length; i++){
+            getHash(i);
+        }
+
+    hashArr.sort(function(b,a){return a.imHash - b.imHash});
+    for(let i = 0; i<parsedNarrL; i++){
+        console.log(hashArr[i]);
+    }
+}
+hashAnalysis();
+
+
+async function getHash(num){
+    await Jimp.read("./aspen/"+getName(num)+".png", function (err, image){
+        // console.log(image.hash(10));
+         hashArr.push({imHash:parseInt(image.hash(10)),imName:getName(num)});
+         
+        // if(num === 77)console.log(hashArr);
+ 
+     });
+}
+
+
