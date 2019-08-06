43437232208ed008c2c25fbd9d306f66db61a59a
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-08-05 23:14:56 -0600

    attempted to add Socket.io
    Stuck on the dumbest error....:(
    -MAx

--- serverTest.js
+++ serverTest.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var http = require('http').createServer(app);
 let fs = require('fs');
+var io = require('socket.io')(http);
 
 var name;
 var bodyParser = require("body-parser");
@@ -9,12 +11,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/index.html');
 });
-
+io.on('connection', function(socket){
+    console.log('a user connected');
+  });
 app.post('/submit-student-data', function (req, res) {
     name = req.body.firstName + ' ' + req.body.lastName;
     
 //    res.send(name + ' Submitted Successfully!');
-    var img = fs.readFileSync('./aspen/2017+06Jun+23+18+0+0Lan[TR].png');
+    if(name === "TEST test"){
+        
+      var img = fs.readFileSync('./aspen/2017+06Jun+23+18+0+0Lan[TR].png');
+    
+    }
+    else{
+        var img = fs.readFileSync('./aspen/2017+04Apr+20+18+0+0Lan[TR].png');
+
+    }
     res.writeHead(200, {'Content-Type': 'image/gif' });
     res.end(img, 'binary');
     
