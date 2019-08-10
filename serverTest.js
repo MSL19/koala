a8d1abfeaee6c18c5f30de87d6249b67586159d2
MSL19 <40368820+MSL19@users.noreply.github.com>
2019-08-10 16:40:44 -0600

    Frist steps of Server to Client interaction
    Fugured out how to pull down JSON on the server side and then send it to the client (this is the first step twards server analysis and client display process). Remember to make 2 different http vars to seperate server hosting from data grab. Also figured out how to display 2 images from server to cleint at once.
    -MAx

--- serverTest.js
+++ serverTest.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+let http2 = require("http");
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 let fs = require('fs');
@@ -6,16 +7,55 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
+io.on('connection', async function(socket){
+    socket.on('chat message', async function(msg){
       console.log('message: ' + msg);
       fs.readFile('./aspen/2016+09Sep+24+18+0+0Lan[TR].png', function(err, data){
         socket.emit('imageConversionByClient', { image: true, buffer: data });
         socket.emit('imageConversionByServer', "data:image/png;base64,"+ data.toString("base64"));
       });
+      fs.readFile('./aspen/2016+10Oct+10+18+0+0Lan[TR].png', function(err, data){
+        socket.emit('imageConversionByClient2', { image: true, buffer: data });
+        socket.emit('imageConversionByServer2', "data:image/png;base64,"+ data.toString("base64"));
+      });
+      let JSONdata = await getSatData("1401087271",Math.floor(Date.now()/1000),"5d0d454d6dae90004761d30a","cda3de7380c305987a9346a110328670");
+      let data = JSON.stringify(JSONdata);
+
+      io.emit('chat message', data);
+
     });
+    
+    
   });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function getSatData(startDate,endDate,polyId,APId){ //this pulls the JSON data on Apple stock from Alphavantage and returns the JSON
+  return new Promise(function(resolve, reject){
+      let request = http2.request({
+          method: "GET",
+          host: "api.agromonitoring.com", //"api.intrinio.com",
+          path: "/agro/1.0/image/search?start="+startDate+"&end="+endDate+"&polyid="+polyId+"&appid="+APId, 
+          
+      
+      }, function(response) {
+          let json = "";
+          response.on('data', function (chunk) {
+              json += chunk;
+          });
+          response.on('end', function() {
+              try{
+              company = JSON.parse(json);   
+              resolve(company); //returning the JSON
+              }
+              catch(e){
+              reject(e);
+              }                
+          });
+      });
+      request.end();
+      });
+       
+}
